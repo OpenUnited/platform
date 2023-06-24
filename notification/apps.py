388f5a67c8f6af91cdeaee9e29ffca4d05cb7c9e
Adrian McPhee <adrianmcphee@amc.home>
2023-06-24 18:44:26 +0200

    Consolidate 'Demand' domain into Product and Talent

--- notification/apps.py
+++ notification/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DemandConfig(AppConfig):
+class NotificationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'demand'
+    name = 'notification'
