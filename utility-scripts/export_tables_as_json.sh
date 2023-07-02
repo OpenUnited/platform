2411126e98e9b57389c7aaa3c3805c19846c3a6c
Adrian McPhee <adrianmcphee@amc.home>
2023-07-02 15:35:05 +0200

    Script to dump database as json files and other small tweaks

---
+++ utility-scripts/export_tables_as_json.sh
@@ -0,0 +1,13 @@
+SCHEMA="public"
+DB="openunited"
+CURRENT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+EXPORT_DIR="$CURRENT_DIR/export"  
+
+mkdir -p $EXPORT_DIR
+
+psql -Atc "select tablename from pg_tables where schemaname='$SCHEMA'" $DB |\
+  while read TBL; do
+    psql -d $DB -qAtX -c "select json_agg(t) FROM (SELECT * from $SCHEMA.$TBL) t;" -o $EXPORT_DIR/$TBL.json
+  done
+
+
