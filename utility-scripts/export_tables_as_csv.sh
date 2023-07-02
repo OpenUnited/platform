795b235c6573afd9bebfcc16b183d86149b8b2f6
Adrian McPhee <adrianmcphee@amc.home>
2023-07-02 17:32:14 +0200

    Export tables to CSV

---
+++ utility-scripts/export_tables_as_csv.sh
@@ -0,0 +1,13 @@
+SCHEMA="public"
+DB="openunited"
+CURRENT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+EXPORT_DIR="$CURRENT_DIR/export"  
+
+mkdir -p $EXPORT_DIR
+
+psql -Atc "select tablename from pg_tables where schemaname='$SCHEMA'" $DB |\
+  while read TBL; do
+    psql -c "COPY $SCHEMA.$TBL TO '$EXPORT_DIR/$TBL.csv' DELIMITER ',' CSV HEADER;" $DB
+  done
+
+
