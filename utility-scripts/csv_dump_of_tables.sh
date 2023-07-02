ce70f06c313c5cbe58a9a34a66623e84a3e4f53e
Adrian McPhee <adrianmcphee@amc.home>
2023-07-02 20:35:22 +0200

    Tweaks to db dump files

--- utility-scripts/csv_dump_of_tables.sh
+++ utility-scripts/csv_dump_of_tables.sh
@@ -1,8 +1,10 @@
 SCHEMA="public"
 DB="openunited"
 CURRENT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-EXPORT_DIR="$CURRENT_DIR/export"  
 
+EXPORT_DIR="$CURRENT_DIR/export"
+mkdir -p $EXPORT_DIR
+EXPORT_DIR="$EXPORT_DIR/csv"
 mkdir -p $EXPORT_DIR
 
 psql -Atc "select tablename from pg_tables where schemaname='$SCHEMA'" $DB |\
