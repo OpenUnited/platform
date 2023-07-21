54285fa23ac2147c52a7606104bf4958d04afc91
Doğukan Teber <dogukanteber1@hotmail.com>
2023-07-21 15:20:34 +0300

    Avoid code repetition in forms
    Signed-off-by: Doğukan Teber <dogukanteber1@hotmail.com>

--- talent/forms.py
+++ talent/forms.py
@@ -1,61 +1,47 @@
 from django import forms
-from django.forms import TextInput, EmailInput, PasswordInput
 from django.contrib.auth.forms import UserCreationForm
 
 from security.models import User
 
 
+def styled_input(input_type, placeholder):
+    """
+    This function returns a TextInput or PasswordInput widget with predefined style.
+    """
+    attrs = {
+        "class": "my-2 g-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
+        "placeholder": placeholder,
+    }
+
+    if input_type == "password":
+        return forms.PasswordInput(attrs=attrs)
+    elif input_type == "email":
+        return forms.EmailInput(attrs=attrs)
+    else:
+        return forms.TextInput(attrs=attrs)
+
+
 class SignUpForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(SignUpForm, self).__init__(*args, **kwargs)
-        self.fields["password1"].widget = PasswordInput(
-            attrs={
-                "class": "my-2 g-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
-                "placeholder": "Password",
-            }
-        )
-        self.fields["password2"].widget = PasswordInput(
-            attrs={
-                "class": "my-2 g-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
-                "placeholder": "Confirm Password",
-            }
-        )
+        self.fields["password1"].widget = styled_input("password", "Password")
+        self.fields["password2"].widget = styled_input("password", "Confirm Password")
 
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
         widgets = {
-            "username": TextInput(
-                attrs={
-                    "class": "my-2 g-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
-                    "placeholder": "Username",
-                }
-            ),
-            "email": EmailInput(
-                attrs={
-                    "class": "my-2 g-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
-                    "placeholder": "Email",
-                }
-            ),
+            "username": styled_input("text", "Username"),
+            "email": styled_input("email", "Email"),
         }
 
 
 class SignInForm(forms.Form):
     username = forms.CharField(
         max_length=65,
-        widget=forms.TextInput(
-            attrs={
-                "class": "my-2 g-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
-                "placeholder": "Username",
-            }
-        ),
+        widget=styled_input("text", "Username"),
     )
     password = forms.CharField(
         max_length=65,
-        widget=forms.PasswordInput(
-            attrs={
-                "class": "my-2 g-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
-                "placeholder": "Password",
-            }
-        ),
+        widget=styled_input("password", "Password"),
     )
