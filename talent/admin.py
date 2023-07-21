c5d7805e8211f3f3970012e4efc579544ea580ef
Doğukan Teber <dogukanteber1@hotmail.com>
2023-07-21 15:29:54 +0300

    Create an admin view for Person model
    Signed-off-by: Doğukan Teber <dogukanteber1@hotmail.com>

--- talent/admin.py
+++ talent/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Person
+
+
+class PersonAdmin(admin.ModelAdmin):
+    exclude = ("id",)
+
+
+admin.site.register(Person, PersonAdmin)
