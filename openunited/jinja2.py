d5cd717cd624fa9f91e0c384770a7f05b6c2182c
Doğukan Teber <dogukanteber1@hotmail.com>
2023-07-26 12:35:30 +0300

    Set up Jinja template engine
    Signed-off-by: Doğukan Teber <dogukanteber1@hotmail.com>

---
+++ openunited/jinja2.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import  # Python 2 only
+
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.urls import reverse
+
+from jinja2 import Environment
+
+
+def environment(**options):
+    env = Environment(**options)
+    env.globals.update(
+        {
+            "static": staticfiles_storage.url,
+            "url": reverse,
+        }
+    )
+    return env
