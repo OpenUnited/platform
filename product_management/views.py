301b587176425eb0d58d74ed3ec3f1ec15c6b02b
Doğukan Teber <dogukanteber1@hotmail.com>
2023-07-31 16:52:26 +0300

    Set up urls for product detail and implement product summary page except the product tree
    Signed-off-by: Doğukan Teber <dogukanteber1@hotmail.com>

--- product_management/views.py
+++ product_management/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse, render, redirect
+from django.urls import reverse
 from django.views.generic import ListView
 
 from .models import Challenge, Product
@@ -35,3 +36,92 @@ class ProductListView(ListView):
         response = super().get(request, *args, **kwargs)
 
         return response
+
+
+def product_redirect(request, organisation_username, product_slug):
+    kwargs = {
+        "organisation_username": organisation_username,
+        "product_slug": product_slug,
+    }
+    url = reverse("product_summary", kwargs=kwargs)
+
+    return redirect(url)
+
+
+def product_detail(request, organisation_username, product_slug):
+    return render(
+        request,
+        "product_management/product_detail_base.html",
+        context={
+            "organisation_username": organisation_username,
+            "product_slug": product_slug,
+        },
+    )
+
+
+def product_summary(request, organisation_username, product_slug):
+    product = Product.objects.get(slug=product_slug)
+    return render(
+        request,
+        "product_management/product_summary.html",
+        context={
+            "organisation_username": organisation_username,
+            "product_slug": product_slug,
+            "product": product,
+        },
+    )
+
+
+def product_initiatives(request, organisation_username, product_slug):
+    return render(
+        request,
+        "product_management/product_detail_base.html",
+        context={
+            "organisation_username": organisation_username,
+            "product_slug": product_slug,
+        },
+    )
+
+
+def product_challenges(request, organisation_username, product_slug):
+    return render(
+        request,
+        "product_management/product_detail_base.html",
+        context={
+            "organisation_username": organisation_username,
+            "product_slug": product_slug,
+        },
+    )
+
+
+def product_tree(request, organisation_username, product_slug):
+    return render(
+        request,
+        "product_management/product_detail_base.html",
+        context={
+            "organisation_username": organisation_username,
+            "product_slug": product_slug,
+        },
+    )
+
+
+def product_ideas_bugs(request, organisation_username, product_slug):
+    return render(
+        request,
+        "product_management/product_detail_base.html",
+        context={
+            "organisation_username": organisation_username,
+            "product_slug": product_slug,
+        },
+    )
+
+
+def product_people(request, organisation_username, product_slug):
+    return render(
+        request,
+        "product_management/product_detail_base.html",
+        context={
+            "organisation_username": organisation_username,
+            "product_slug": product_slug,
+        },
+    )
