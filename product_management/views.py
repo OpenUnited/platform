db8c417e34da5bf9942368d33ed916434c00d540
Doğukan Teber <dogukanteber1@hotmail.com>
2023-08-02 16:01:22 +0300

    Check if the given URL is valid if not, display 404 page
    Signed-off-by: Doğukan Teber <dogukanteber1@hotmail.com>

--- product_management/views.py
+++ product_management/views.py
@@ -7,6 +7,7 @@ from django.db import models
 from django.views.generic import ListView, TemplateView, RedirectView
 
 from .models import Challenge, Product, Initiative, Bounty, Capability
+from commerce.models import Organisation
 
 
 class ChallengeListView(ListView):
@@ -41,10 +42,17 @@ class BaseProductDetailView:
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
+        organisation = get_object_or_404(
+            Organisation, username=self.kwargs["organisation_username"]
+        )
+        product = get_object_or_404(Product, slug=self.kwargs["product_slug"])
+
         context.update(
             {
-                "organisation_username": self.kwargs["organisation_username"],
-                "product_slug": self.kwargs["product_slug"],
+                "organisation": organisation,
+                "organisation_username": organisation.username,
+                "product": product,
+                "product_slug": product.slug,
             }
         )
 
@@ -63,11 +71,10 @@ class ProductSummaryView(BaseProductDetailView, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        product = get_object_or_404(Product, slug=self.kwargs["product_slug"])
+        product = context["product"]
         challenges = Challenge.objects.filter(product=product)
         context.update(
             {
-                "product": product,
                 "challenges": challenges,
                 "capabilities": Capability.get_root_nodes(),
             }
@@ -80,7 +87,7 @@ class ProductChallengesView(BaseProductDetailView, TemplateView):
 
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         context = super().get_context_data(**kwargs)
-        product = get_object_or_404(Product, slug=self.kwargs["product_slug"])
+        product = context["product"]
         challenges = Challenge.objects.filter(product=product)
         context.update(
             {
@@ -96,7 +103,7 @@ class ProductInitiativesView(BaseProductDetailView, TemplateView):
 
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         context = super().get_context_data(**kwargs)
-        product = get_object_or_404(Product, slug=self.kwargs["product_slug"])
+        product = context["product"]
         initiatives = Initiative.objects.filter(product=product)
 
         # Query to calculate total points for each Initiative, considering only active Bounties with status "Available"
@@ -112,7 +119,6 @@ class ProductInitiativesView(BaseProductDetailView, TemplateView):
 
         context.update(
             {
-                "product": product,
                 "initiatives": initiatives,
             }
         )
