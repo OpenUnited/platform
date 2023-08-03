445f1cee0db250dda3d686e6aa0de161e9ccfecc
Doğukan Teber <dogukanteber1@hotmail.com>
2023-08-03 12:28:24 +0300

    Undo the previous sign up and sign in implementation
    Signed-off-by: Doğukan Teber <dogukanteber1@hotmail.com>

--- talent/urls.py
+++ talent/urls.py
@@ -3,9 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path("signin", views.sign_in, name="sign_in"),
-    path("signup", views.sign_up, name="sign_up"),
     path("logout", views.log_out, name="log_out"),
-    path("reset_password", views.reset_password, name="reset_password"),
-    path("complete_profile", views.complete_profile, name="complete_profile"),
 ]
--- talent/views.py
+++ talent/views.py
@@ -2,64 +2,7 @@ from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
-from .forms import SignUpForm, SignInForm, PersonDetailsForm
-from .services import PersonService
-
-
-def sign_in(request):
-    if request.method == "POST":
-        form = SignInForm(request.POST)
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect("home")
-        else:
-            form.add_error(None, "Invalid credentials")
-    else:
-        form = SignInForm()
-
-    return render(request, "talent/sign_in.html", {"form": form})
-
-
-# TODO: add unauthorized page when someone tries to reach this view
-# TODO: saving photos does not work right now, fix it
-@login_required
-def complete_profile(request):
-    if request.method == "POST":
-        form = PersonDetailsForm(request.POST)
-        if form.is_valid():
-            profile = PersonService.get_by_username(request.user.username)
-            PersonService.update(profile=profile, **form.cleaned_data)
-
-            return redirect("home")
-    return render(request, "talent/sign_up_details.html", {"form": PersonDetailsForm()})
-
-
-def sign_up(request):
-    if request.method == "POST":
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get("username")
-            raw_password = form.cleaned_data.get("password1")
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect("complete_profile")
-        else:
-            return render(request, "talent/sign_up.html", {"form": form})
-
-    form = SignUpForm()
-    return render(request, "talent/sign_up.html", {"form": form})
-
 
 def log_out(request):
     logout(request)
     return redirect("home")
-
-
-def reset_password(request):
-    return HttpResponse(
-        "Implement this page. Check if Django provides the necessary functions"
-    )
