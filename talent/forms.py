187d2ef27d0e49cac0763d0fd02b144365fae883
Doğukan Teber <dogukanteber1@hotmail.com>
2023-08-07 13:45:23 +0300

    Add password strength checker
    Signed-off-by: Doğukan Teber <dogukanteber1@hotmail.com>

--- talent/forms.py
+++ talent/forms.py
@@ -87,6 +87,7 @@ class SignUpStepThreeForm(forms.Form):
     password = forms.CharField(
         widget=forms.PasswordInput(
             attrs={
+                "id": "password-checker-field",
                 "class": "block w-full h-10 rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-gray-300 sm:text-sm sm:leading-6 focus-visible:outline-transparent",
                 "placeholder": "••••••",
                 "name": "password",
