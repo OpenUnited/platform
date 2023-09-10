0f9cadbd8c3fa340971ca2e4ad71e817805d9de4
Doğukan Teber <dogukanteber1@hotmail.com>
2023-09-10 15:29:59 +0300

    Add unit tests for security/views
    Signed-off-by: Doğukan Teber <dogukanteber1@hotmail.com>

--- security/tests/factories.py
+++ security/tests/factories.py
@@ -8,6 +8,7 @@ class UserFactory(DjangoModelFactory):
     first_name = factory.Sequence(lambda n: f"first_name{n}")
     last_name = factory.Sequence(lambda n: f"last_name{n}")
     username = factory.Sequence(lambda n: f"username{n}")
+    password = factory.Sequence(lambda n: f"password{n}")
     email = factory.LazyAttribute(lambda obj: f"{obj.username}@example.com")
 
     class Meta:
