9b3923ec327e89c189b44e5b7ba6b728939ad3b7
Francisco Javier Lendínez Tirado <fjlendinez@gmail.com>
2023-09-30 10:44:39 +0200

    Update tailwind.config.js to handle multiple levels of folders

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./templates/*.html', './templates/*/*.html', './*/templates/*/*.html'],
+  content: ['./templates/*.html', './templates/**/*.html', './*/templates/**/*.html'],
   theme: {
     extend: {},
   },
