d17ffc11b049d9220c273063ab77b61aeeb9ce8e
Doğukan Teber <dogukanteber1@hotmail.com>
2023-10-04 13:04:44 +0300

    Add reviewer field to the challenge form,
    Signed-off-by: Doğukan Teber <dogukanteber1@hotmail.com>

--- product_management/forms.py
+++ product_management/forms.py
@@ -10,6 +10,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from commerce.models import Organisation
 from talent.models import BountyClaim, Person
 from .models import Idea, Product, Challenge, Bounty
+from security.models import ProductRoleAssignment
 
 
 class DateInput(forms.DateInput):
@@ -229,6 +230,19 @@ class ChallengeForm(forms.ModelForm):
     product = forms.ModelChoiceField(
         empty_label="Select a product",
         queryset=Product.objects.all(),
+        widget=forms.Select(
+            attrs={
+                "class": "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6",
+                "hx-get": reverse_lazy("get-people-of-product"),
+                "hx-target": "#id_reviewer",
+                "hx-swap": "outerHTML",
+            }
+        ),
+    )
+    # TODO: limit this with ProductRoleAssignment
+    reviewer = forms.ModelChoiceField(
+        empty_label="Select a reviewer",
+        queryset=ProductRoleAssignment.objects.none(),
         widget=forms.Select(
             attrs={
                 "class": "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6",
