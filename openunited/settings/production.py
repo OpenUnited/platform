0db3ff826a6b24226e2c65d3db0077d2b5308708
andreparames <github@andreparames.com>
2023-10-18 18:42:37 +0100

    fix: load sentry DSN from environment
    Hardcoding the value in the codebase meant everyone using
    the production settings would get their info sent to the
    same Sentry account.

--- openunited/settings/production.py
+++ openunited/settings/production.py
@@ -57,9 +57,10 @@ LOGGING = {
     },
 }
 
-sentry_sdk.init(
-    dsn="https://34dd2db529445f60bd209e142e98af22@o1120097.ingest.sentry.io/4506064626909184",
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    traces_sample_rate=1.0,
-)
+if os.environ.get("SENTRY_DSN"):
+    sentry_sdk.init(
+        dsn=os.environ.get("SENTRY_DSN"),
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        traces_sample_rate=1.0,
+    )
