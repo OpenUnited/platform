842960c2e49e8cb14b660eeaa1733592b28b0061
Adrian McPhee <adrian@openunited.com>
2023-10-22 10:34:24 +0200

    Revert "Set up S3 object storage"

--- openunited/settings/base.py
+++ openunited/settings/base.py
@@ -26,8 +26,7 @@ INSTALLED_APPS = [
     "django_jinja",
     "formtools",
     "debug_toolbar",
-    "storages",
-    "dbbackup",
+    'dbbackup',
     "openunited_backup",
 ]
 
@@ -115,12 +114,12 @@ DATABASES = {
 }
 
 if os.environ.get("DBBACKUP_BUCKET"):
-    DBBACKUP_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    DBBACKUP_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     DBBACKUP_STORAGE_OPTIONS = {
-        "access_key": os.environ.get("DBBACKUP_ACCESS_KEY"),
-        "secret_key": os.environ.get("DBBACKUP_SECRET_KEY"),
-        "bucket_name": os.environ.get("DBBACKUP_BUCKET"),
-        "default_acl": "private",
+        'access_key': os.environ.get("DBBACKUP_ACCESS_KEY"),
+        'secret_key': os.environ.get("DBBACKUP_SECRET_KEY"),
+        'bucket_name': os.environ.get("DBBACKUP_BUCKET"),
+        'default_acl': 'private',
     }
 
 AUTH_USER_MODEL = "security.User"
