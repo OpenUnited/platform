fb65b767fb2db98c4723ed6b520a17c4868c07f0
Doğukan Teber <dogukanteber1@hotmail.com>
2023-10-31 13:25:11 +0300

    style: format unformatted files
    Signed-off-by: Doğukan Teber <dogukanteber1@hotmail.com>

--- commerce/apps.py
+++ commerce/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class CommerceConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'commerce'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "commerce"
