93ded51cde6eaf9fd22cf97b9bbc46aaa85245f0
Doğukan Teber <dogukanteber1@hotmail.com>
2023-11-02 16:41:38 +0300

    refactor: remove unused attributes
    Signed-off-by: Doğukan Teber <dogukanteber1@hotmail.com>

--- product_management/forms.py
+++ product_management/forms.py
@@ -275,9 +275,6 @@ class ChallengeForm(forms.ModelForm):
         widget=forms.Select(
             attrs={
                 "class": "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6",
-                # "hx-get": reverse_lazy("get-people-of-product"),
-                # "hx-target": "#id_reviewer",
-                # "hx-swap": "outerHTML",
             }
         ),
     )
