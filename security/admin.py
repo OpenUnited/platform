85ce6d28e889577feebbb8353c6369eab3283dec
endalkh <endalkhbel@gmail.com>
2024-03-24 23:30:12 +0300

    feat: register ProductRoleAssignmentAdmin

--- security/admin.py
+++ security/admin.py
@@ -13,7 +13,22 @@ admin.site.register(
         User,
         SignInAttempt,
         SignUpRequest,
-        ProductRoleAssignment,
         BlacklistedUsernames,
     ]
 )
+
+
+@admin.register(ProductRoleAssignment)
+class ProductRoleAssignmentAdmin(admin.ModelAdmin):
+    def product_name(self, obj):
+        return obj.product.name
+
+    def person_name(self, obj):
+        return obj.person.user
+
+    list_display = ["pk", "product_name", "person_name", "role"]
+    search_fields = [
+        "person__user__username",
+        "person__user__email",
+        "product__name",
+    ]
