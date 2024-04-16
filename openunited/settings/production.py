a7c3d3ceb5ab3ab287950b22d16fa80aea330d44
RuzgarEser <120990068+RuzgarEser@users.noreply.github.com>
2024-04-16 16:09:59 +0000

    Fixed test-prs.yml github actions.

--- openunited/settings/production.py
+++ openunited/settings/production.py
@@ -32,7 +32,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 LOGGING = {
