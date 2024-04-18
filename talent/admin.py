4e91979e18b96e70f294b86350a33476960fa336
endalkh <endalkhbel@gmail.com>
2024-04-18 04:23:38 +0300

    feat: add models to admin site.

--- talent/admin.py
+++ talent/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
-from .models import Person, Status, Feedback
+from .models import Person, Status, Feedback, BountyClaim
 
-admin.site.register([Person, Status, Feedback])
+admin.site.register([Person, Status, Feedback, BountyClaim])
