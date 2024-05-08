cbb91a7758183567152b4daf2d7a52c81cc50a63
endalkh <endalkhbel@gmail.com>
2024-05-08 13:16:44 +0300

    feat: add bounty filter

--- product_management/filters.py
+++ product_management/filters.py
@@ -3,7 +3,7 @@ from django.db import models
 from security.models import ProductRoleAssignment
 
 import django_filters
-from product_management.models import Challenge
+from product_management.models import Challenge, Bounty, Skill, Expertise
 
 register = template.Library()
 
@@ -49,4 +49,38 @@ class ChallengeFilter(django_filters.FilterSet):
             "status",
             "priority",
             "reward_type",
+        ]
+
+
+class BountyFilter(django_filters.FilterSet):
+    class_name = "w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+    status = django_filters.ChoiceFilter(
+        choices=Bounty.BOUNTY_STATUS,
+        initial="Draft",
+        empty_label="All",
+        widget=forms.Select(
+            attrs={"class": class_name},
+        ),
+    )
+    skill = django_filters.ModelChoiceFilter(
+        queryset=Skill.objects.all(),
+        empty_label="All",
+        widget=forms.Select(
+            attrs={"class": class_name},
+        ),
+    )
+    expertise = django_filters.ModelChoiceFilter(
+        queryset=Expertise.objects.all(),
+        empty_label="All",
+        widget=forms.Select(
+            attrs={"class": class_name},
+        ),
+    )
+
+    class Meta:
+        model = Bounty
+        fields = [
+            "status",
+            "skill",
+            "expertise",
         ]
