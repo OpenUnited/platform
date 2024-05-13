de3c841937990a7a636b4ee0ddddd7735f773cbb
endalkh <endalkhbel@gmail.com>
2024-05-13 18:56:24 +0300

    feat: add search fields and filter.

--- product_management/admin.py
+++ product_management/admin.py
@@ -32,6 +32,6 @@ class ChallengeAdmin(admin.ModelAdmin):
 @admin.register(product.Bounty)
 class BountyAdmin(admin.ModelAdmin):
     list_display = ["pk", "title", "status"]
-    list_filter = ["is_active"]
-    search_fields = ["is_active"]
+    list_filter = ["is_active", "status"]
+    search_fields = ["title"]
     filter_horizontal = ["expertise"]
