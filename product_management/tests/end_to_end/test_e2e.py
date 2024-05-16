7b108959fb1089d3449a952cca29e3ad36b886d1
Sachin Tom <sachin.tom.dev@gmail.com>
2024-05-16 10:31:02 +0530

    feat: added setup files

---
+++ product_management/tests/end_to_end/test_e2e.py
@@ -0,0 +1,3 @@
+from playwright.sync_api import sync_playwright
+from .fixtures import django_server  # (Assuming `django_server` fixture exists)
+from .pages import LoginPage  # (If using page objects)
