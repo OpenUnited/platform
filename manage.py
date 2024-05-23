e0c241bbc261ef027725e733da9757371b00a7f7
endalkh <endalkhbel@gmail.com>
2024-05-23 18:12:13 +0300

    chore: add default local setting

--- manage.py
+++ manage.py
@@ -7,7 +7,8 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE", os.environ.get("DJANGO_SETTINGS_MODULE")
+        "DJANGO_SETTINGS_MODULE",
+        os.environ.get("DJANGO_SETTINGS_MODULE", "openunited.settings.local"),
     )
     try:
         from django.core.management import execute_from_command_line
