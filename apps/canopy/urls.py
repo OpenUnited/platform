a9bb59153f69b7d87744ddd1dee391d5ae2481d1
endalkh <endalkhbel@gmail.com>
2024-06-04 14:07:16 +0300

    feat: add multiple tree roots

--- apps/canopy/urls.py
+++ apps/canopy/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("", views.introduction, name="introduction"),
     path("chapter-1/", views.chapter1, name="chapter1"),
     path("update-node/<int:pk>", views.update_node, name="update_node"),
-    path("add-node/<int:parent_id>", views.add_node, name="add_node"),
+    path("add-node/<str:tree_id>/root", views.add_root_node, name="add_node_root"),
+    path("add-node/<int:parent_id>/child", views.add_node, name="add_node"),
     path("delete-node/<int:pk>", views.delete_node, name="delete_node"),
 ]
