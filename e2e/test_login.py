c679f0395d3b7571929563d7493aca1eaaec66f0
Sachin Tom <sachin.tom.dev@gmail.com>
2024-06-10 10:17:22 +0530

    feat: refactored tests to function based, added fixtures

--- e2e/test_login.py
+++
@@ -1,9 +0,0 @@
-from e2e.base import BaseE2ETest
-
-class TestLogin(BaseE2ETest):
-    def setUp(self):
-        super().setUp()
-
-
-    def test_login(self):
-        self.assertTrue(self.page.is_visible("#navbar-menu-button"))
---
+++ e2e/tests/test_signup.py
@@ -0,0 +1,7 @@
+from e2e.pages.signup_page import SignupPage
+
+def test_signup(live_server, page_context):
+    signup_page = SignupPage(page_context)
+    signup_page.navigate(f"{live_server.url}{signup_page.url}")
+    signup_page.signup("Pacey Witter", "pacey@gmail.com", "pacey", "pacey", "4@Password")
+    assert page_context.is_visible("#navbar-menu-button")
