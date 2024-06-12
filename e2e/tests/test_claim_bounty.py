397975ea0bdb0a5e983d8121d5768c36b3d948b7
Endalk Belete <54709447+endalkh@users.noreply.github.com>
2024-06-12 15:52:44 +0300

    Update test_claim_bounty.py

--- e2e/tests/test_claim_bounty.py
+++ e2e/tests/test_claim_bounty.py
@@ -38,6 +38,8 @@ def test_claim_bounty(live_server, page_context, setup_bounty):
     page.request_claim_button.click()
 
     page_context.wait_for_timeout(1500)
-
+    for i in  BountyClaim.objects.all():
+        print("===================:",  i.bounty)
+        
     bounty_claim = BountyClaim.objects.get(bounty=bounty)
     assert bounty_claim.status == BountyClaim.Status.REQUESTED
