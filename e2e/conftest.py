a02eff00a5633c5789569716aab78681f1ceece6
endalkh <endalkhbel@gmail.com>
2024-06-12 15:35:19 +0300

    feat: add e2e folder

--- e2e/conftest.py
+++
@@ -1,1 +0,0 @@
-from e2e.fixtures import *
--- e2e/helpers.py
+++
@@ -1,7 +0,0 @@
-from e2e.pages.login_page import LoginPage
-
-
-def login_user(page, live_server_url, username, password):
-    login_page = LoginPage(page)
-    login_page.navigate(f"{live_server_url}{login_page.url}")
-    login_page.login(username, password)
