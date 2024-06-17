7b185e83f3c2983bae8c91022c5f10819a081551
endalkh <endalkhbel@gmail.com>
2024-06-17 21:43:11 +0300

    feat: change the url patern of the tree

--- apps/canopy/views.py
+++ apps/canopy/views.py
@@ -1,12 +1,11 @@
 import uuid
 
 from django.http import JsonResponse
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.views import generic
 
 from apps.canopy import utils
-from apps.common import utils as common_utils
 from apps.product_management import models as mgt
 
 
@@ -18,54 +17,39 @@ def chapter1(request):
     return render(request, "chapter-1.html")
 
 
-def share_tree_link(request):
-    return redirect(f'{reverse("shareable_product_tree")}?key={request.GET.get("key", None)}')
-
-
 class ProductTreeView(generic.CreateView):
     template_name = "unauthenticated_tree/index.html"
     model = mgt.ProductTree
     fields = ["name"]
 
     def get_context_data(self, **kwargs):
-        if key := self.request.GET.get("key", None):
-            product_tree = get_object_or_404(mgt.ProductTree, pk=key)
-            show_share_button = False
-
+        if "tree_session_id" not in self.request.session:
+            session_id = str(uuid.uuid4())
+            self.request.session["tree_session_id"] = session_id
         else:
-            if "tree_session_id" not in self.request.session:
-                session_id = str(uuid.uuid4())
-                self.request.session["tree_session_id"] = session_id
-            else:
-                session_id = self.request.session["tree_session_id"]
+            session_id = self.request.session["tree_session_id"]
 
-            product_tree = mgt.ProductTree.objects.filter(session_id=session_id).first()
+        product_tree = mgt.ProductTree.objects.filter(session_id=session_id).first()
 
-            if not product_tree:
-                product_tree = mgt.ProductTree.objects.create(name=utils.generate_unique_name(), session_id=session_id)
-                mgt.ProductArea.add_root(
-                    name="Product Area",
-                    description="Description of Product Area",
-                    product_tree=product_tree,
-                )
-            show_share_button = True
+        if not product_tree:
+            product_tree = mgt.ProductTree.objects.create(name=utils.generate_unique_name(), session_id=session_id)
+            mgt.ProductArea.add_root(
+                name="Product Area",
+                description="Description of Product Area",
+                product_tree=product_tree,
+            )
+        show_share_button = True
 
-        domain = f"{self.request.scheme}://{self.request.get_host()}"
-        return {
-            "can_modify_product": True,
-            "product_tree": product_tree,
-            "sharable_link": f"{domain}/product-tree/share/?key={product_tree.pk}",
-            "tree_data": [common_utils.serialize_tree(node) for node in product_tree.product_areas.filter(depth=1)],
-            "show_share_button": show_share_button,
-            "margin_left": int(self.request.GET.get("margin_left", 0)),
-            "depth": int(self.request.GET.get("depth", 0)),
-        }
+        return utils.shareable_tree_helper(self.request, product_tree, show_share_button)
 
 
 class ProductTreeUpdateView(generic.UpdateView):
     template_name = "unauthenticated_tree/index.html"
     model = mgt.ProductTree
     fields = ["name"]
+
+    def get_context_data(self, **kwargs):
+        return utils.shareable_tree_helper(self.request, self.get_object())
 
     def form_valid(self, form):
         obj = form.save()
