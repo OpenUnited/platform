da10714e1f6ea52c20cc9c72d153ef8c0eb731a1
endalkh <endalkhbel@gmail.com>
2024-06-18 16:10:32 +0300

    feat: add search to user managment

---
+++ apps/static/scripts/searchable_dropdown.js
@@ -0,0 +1,39 @@
+function searchableDropdownFilterInput(event){
+    const filterInput = document.getElementById('searchable-dropdown-input');
+    const dropdownList = document.getElementById('searchable-dropdown-list');
+    const value = filterInput.value.toLowerCase();
+    const listItems = dropdownList.children;
+
+    for (let i = 0; i < listItems.length; i++) {
+      const listItem = listItems[i];
+      console.log(listItem)
+
+      if (listItem.textContent.toLowerCase().includes(value)) {
+        listItem.classList.remove("hidden");
+      } else {
+        listItem.classList.add("hidden");
+      }
+    }
+}
+
+function selectItem(event){
+  const listItem = event.target
+
+  // Iterate over the NodeList and remove each element
+  document.querySelectorAll(".searchable-dropdown-li").forEach(icon => icon.classList.remove("bg-gray-100"));
+
+  listItem.classList.add('bg-gray-100');
+
+  document.getElementById('selectfield-selectfield').value = listItem.id
+  document.getElementById('searchable-dropdown').classList.toggle('hidden');
+  document.getElementById('searchable-dropdown-label').textContent = event.target.textContent || 'Please Select';
+
+}
+
+function toggleMenu(event) {
+  document.getElementById('searchable-dropdown').classList.toggle('hidden');
+}
+
+window.searchableDropdownFilterInput = searchableDropdownFilterInput
+window.selectItem = selectItem
+window.toggleMenu = toggleMenu
