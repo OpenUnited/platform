44cf0fb509ebb69fd599ca4b27030e248fe4e305
endalkh <endalkhbel@gmail.com>
2024-06-20 00:45:26 +0300

    fix: e2e issues

--- e2e/tests/test_claim_bounty.py
+++ e2e/tests/test_claim_bounty.py
@@ -35,6 +35,7 @@ def test_claim_bounty(live_server, page_context, setup_bounty):
     year = future_date.strftime("%Y")
     print("========================:", datetime.today())
     print("========================:", day, month, year)
+    page.expected_submission_date.clear()
     page.expected_submission_date.type(f"{day}/{month}/{year}")
 
     print(page.expected_submission_date)
