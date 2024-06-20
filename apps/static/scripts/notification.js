3a909f6c02f94c89b6f4e2f39d9b194411be2e86
endalkh <endalkhbel@gmail.com>
2024-06-20 15:01:14 +0300

    fix: e2e issues

--- apps/static/scripts/notification.js
+++ apps/static/scripts/notification.js
@@ -93,7 +93,7 @@ function claimConfirm(event, termConditionUrl) {
             <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="expected_submission_date">
                 Expected Submission Date
             </label>
-            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="expected_submission_date" type="date" placeholder="DD/MM/YY" required>
+            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="expected_submission_date" type="date" placeholder="YY/MM/DD" required>
         </div>
 
         <div class="mt-3 w-full px-3">
