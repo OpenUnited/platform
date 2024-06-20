a37c5b772dcb028d1b85ed06b5d36746e32520e9
Endalk Belete <54709447+endalkh@users.noreply.github.com>
2024-06-20 15:58:43 +0300

    Update test_claim_bounty.py

--- e2e/tests/test_claim_bounty.py
+++ e2e/tests/test_claim_bounty.py
@@ -1,12 +1,11 @@
 from datetime import datetime, timedelta
-from e2e.pages.challenge_details_page import ChallengeDetailPage
 
-from e2e.helpers import login_user
 from apps.talent.models import BountyClaim
+from e2e.helpers import login_user
+from e2e.pages.challenge_details_page import ChallengeDetailPage
 
 
 def test_claim_bounty(live_server, page_context, setup_bounty):
-
     product, challenge, bounty, _, username, password = setup_bounty
     login_user(page_context, live_server.url, username, password)
 
@@ -15,8 +14,9 @@ def test_claim_bounty(live_server, page_context, setup_bounty):
     assert BountyClaim.objects.filter(bounty=bounty.id).count() == 0
 
     page_context.reload()
-    page_context.wait_for_timeout(1500)
+    page_context.wait_for_timeout(5000)
     challenge_detail_button = page.get_challenge_detail_button(product.slug, challenge.id)
+
     challenge_detail_button.click()
     page_context.wait_for_timeout(1500)
 
@@ -24,29 +24,24 @@ def test_claim_bounty(live_server, page_context, setup_bounty):
     bounty_claim_button.click()
 
     page_context.wait_for_timeout(500)
+    page.bounty_add_btn.click()
+    page_context.wait_for_timeout(500)
 
-    future_date = datetime.today() + timedelta(days=10)
+    future_date = datetime.now() + timedelta(days=10)
+    page_context.wait_for_timeout(500)
+
     day = future_date.strftime("%d")
     month = future_date.strftime("%m")
     year = future_date.strftime("%Y")
 
+    # Clear the date input fields before typing
     page.expected_submission_date.fill(f"{year}-{month}-{day}")
-    page.terms_check_box.check()
 
-    page_context.wait_for_timeout(1000)
+    page.terms_check_box.check()
 
     page.request_claim_button.click()
 
     page_context.wait_for_timeout(1500)
-
-    print("Checking if BountyClaim is created")
-    bounty_claim_count = BountyClaim.objects.filter(bounty=bounty.id).count()
-    print(f"BountyClaim count: {bounty_claim_count}")
-
-    if bounty_claim_count == 0:
-        page_context.screenshot(path="screenshot.png")
-        logs = page_context.evaluate("() => console.log(arguments)")
-        print(f"Console logs: {logs}")
 
     bounty_claim = BountyClaim.objects.get(bounty=bounty.id)
     assert bounty_claim.status == BountyClaim.Status.REQUESTED
